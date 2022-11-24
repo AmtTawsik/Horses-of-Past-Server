572ebd97f63d4423ddd616b62b84cf384eabb287
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-24 23:21:54 +0600

    Category Collection added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { response } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,11 +10,50 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5ctwbw8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        const categoriCollection = client.db("HorsesOfPast").collection("Categories");
+        const productCollection = client.db("HorsesOfPast").collection("Products");
+        const bookingCollection = client.db("HorsesOfPast").collection("booking");
+
+        app.get('/categories', async(req,res)=>{
+            const query = {};
+            const cursor = categoriCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+
+        app.get('/categories/:categoryName', async(req,res)=>{
+            const categoryName = req.params.categoryName;
+            const query = { categoryName };
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
+
+        app.post('/booking',async(req,res)=>{
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch((err) => console.error(err));
+
 
 app.get("/", (req, res) => {
-  res.send("Photo-Phactory server is running");
+  res.send("Horses Of Past server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Photo-Phactory server is running on port ${port}`);
+  console.log(`Horses Of Past server is running on port ${port}`);
 });
